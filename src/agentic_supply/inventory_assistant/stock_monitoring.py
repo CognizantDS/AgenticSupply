9dcf7158cde2f4cd8e943004abdcdf5a236d682d
Stephane Gibon Clement <stephane.gibonclement@cognizant.com>
2025-09-01 13:44:41 +0200

    inventory assistant - update to use JSON data

--- src/agentic_supply/inventory_assistant/stock_monitoring.py
+++ src/agentic_supply/inventory_assistant/stock_monitoring.py
@@ -4,36 +4,57 @@ References :
 
 from typing import Dict, List, Tuple, Optional
 from pydantic import BaseModel, Field
+from importlib.resources import files
 
+from agentic_supply import data
 from agentic_supply.utilities.log_utils import set_logging, get_logger
-from agentic_supply.utilities.config import PRODUCT_NAMES
 
 
 set_logging()
 logger = get_logger(__name__)
 
 
-# class Product(BaseModel):
-#     name: PRODUCT_NAMES
-#     measurement_unit: str
-#     safety_level: float
+class Product(BaseModel):
+    name: str
+    safety_level: Optional[float] = Field(default=None)
+    stock_level: Optional[float] = Field(default=None)
 
 
-def get_stock_level(product_name: PRODUCT_NAMES) -> float:
-    product_to_stock: Dict[PRODUCT_NAMES, float] = {"lactic_acid": 12.3, "ascorbic_acid": 5.7}
-    return product_to_stock[product_name]
+class ProductsDB(BaseModel):
+    products: List[Product]
+
+    def get_product(self, name: str) -> Product:
+        return next((elem for elem in self.products if elem.name == name))
 
 
-def get_safety_level(product_name: PRODUCT_NAMES) -> float:
-    product_to_safety: Dict[PRODUCT_NAMES, float] = {"lactic_acid": 15.0, "ascorbic_acid": 3.0}
-    return product_to_safety[product_name]
+class Site(BaseModel):
+    name: str
+    country: str
+    coordinates: List[float]
+    products: List[Product]
+
+    def is_replenishment_needed(self, product_name: str) -> bool:
+        product = next((elem for elem in self.products if elem.name == product_name))
+        product_db = get_products_db()
+        product_ = product_db.get_product(product.name)
+        return product.stock_level <= product_.safety_level
 
 
-def is_replenishment_needed(
-    product_name: Optional[PRODUCT_NAMES] = None, stock_level: Optional[float] = None, safety_level: Optional[float] = None
-) -> bool:
-    if stock_level is None:
-        stock_level = get_stock_level(product_name)
-    if safety_level is None:
-        safety_level = get_safety_level(product_name)
-    return stock_level <= safety_level
+class SitesDB(BaseModel):
+    sites: List[Site]
+
+    def get_site(self, name: str) -> Site:
+        return next((elem for elem in self.sites if elem.name == name))
+
+    def get_sites(self, product_name: str) -> List[Site]:
+        return [elem for elem in self.sites if product_name in [el.name for el in elem.products]]
+
+
+def get_products_db():
+    json_data = files(data).joinpath("products.json").read_text()
+    return ProductsDB.model_validate_json(json_data)
+
+
+def get_sites_db():
+    json_data = files(data).joinpath("sites.json").read_text()
+    return SitesDB.model_validate_json(json_data)
